522abc8d70edf8f4ca11edc42ce696ad92e3e5b6
Antônio Lima Jr <antonio.df.013@gmail.com>
2021-08-14 13:25:39 -0300

    First commit

---
+++ src/main/java/br/com/antonio/api/ApiCertiDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.antonio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCertiDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCertiDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/antonio/api/ApiCertiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.antonio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCertiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
